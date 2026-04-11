cfe90befdb74a9ff621b0b51ded0ecfe03eebd5e
Md. Jubaer Mahmud Sarker <147659414+jmsman3@users.noreply.github.com>
2026-04-11 23:48:31 +0600

    Update __manifest__.py

--- contact_role_filter/__manifest__.py
+++ contact_role_filter/__manifest__.py
@@ -34,7 +34,7 @@ Smart Auto-Role:
 Developed by: Md. Jubaer Mahmud Sarker
 LinkedIn: https://www.linkedin.com/in/jmsman3/
     """,
-    'author': 'The OdooCraft Solution',
+    'author': 'OdooNest',
     'website': 'https://www.linkedin.com/in/jmsman3/',
     'depends': ['sale_management', 'purchase'],
     'data': [
